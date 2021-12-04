d8571d0ddc59c475a3271cd5ee48b10a5e278492
Arda-Berk <95045907+Arda-Berk@users.noreply.github.com>
2021-12-04 16:44:55 +0300

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+title AimBot 2.5
+type ASCII.txt
+set NUM=7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4 7 E C 4
+for %%x in (%NUM%) do (
+        color 0%%x
+    )
+)
+cls
+color 0b
+cls
+python aimbot.py
+pause
